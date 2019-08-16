d45e8f535d48fb8fe96d4429d72238c1a9ccacfd
ReliaMM <1820184044@qq.com>
2019-08-16 10:31:41 +0800

    热力图

---
+++ 问题/热力图/热力图示例/data/heatMapData_0.js
@@ -0,0 +1,18 @@
+var originObj = [{ "radius": 20, "province": "北京市", "city": "北京市", "fileName": "北京经销网络加二网.xlsx", "heatMap": { "常访地": [
+            // ["116.638905002", "40.1044820675", "10"],
+            // ["116.65891323", "40.1348525282", "10"],
+            // ["116.659047886", "40.134511975", "10"],
+            ["116.659266893", "40.1343775185", "10"],
+            ["116.421921283", "40.0767821716", "9"],
+            ["116.453744689", "39.9657580156", "6"],
+            ["116.05167757", "39.6043329859", "5"],
+            ["116.298825063", "39.7031802099", "2"]
+        ], "居住地": [
+            ["116.554484674", "39.7643037881", "12"],
+            ["116.453744689", "39.9657580156", "10"],
+            ["116.05167757", "39.6043329859", "6"],
+            ["116.298825063", "39.7031802099", "5"]
+        ] }, "max": { "常访地": 10, "居住地": 10 } }, { "pointer": [
+        [116.679537226, 39.9919765148, "点测试1"],
+        [116.419670335, 40.2036096693, "点测试2"]
+    ], "icon": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAB8ElEQVRYR8WXwVUbQQyGP5FDuEEHMQfw3jAdQAWQChIqACpIqCCmA6gAUwEvFcTcdpMDLgFu+ADiiWUfCZ7Z0XrnvczVWunTPxpJFrqcga7zkS/ALsIIGLx+PkOZIkx44IqZ3HndisvQAq9yhHKMsN76jXKHMOaBMw9IGqDO+vo1Yxfvi5EpMmcvBdEOUAe/TWYdwzI15my0QcQBls38PUxCiThAod+Bb37NWy1PKcX8LZwwQF/pF1WIXkUYYKhW7T8yZd+4OaSU8/c+YwAThP2sAMoVlRz4AAqdAZ8yA0ypZMcLoFmDN85KWVA8fAWF/meAoVpf386qgnJDJTY//jkxBaxabejkPBeU8tUHsKUHrHCZMzpPfOa3THwAZjVUm2prWSCUeyoJTtF4Kx7qGOEoE8AZlRz7W7FZFmrLxm0WANigFOstzlnQmBWaoxiDxdeEaN8H8qgQzd4g0htRn1pQonfvU8Cs6tE86/wirPLnDPqtZA3mMuNZOaGScaqI01fwBtGlPf+klN1UcF8NNF42dcQHfnmc8sgOf2TqsfUrUPcGz54Y3f+694HQF22TMjLx2pTopoB5aruKDtL7n2EIP3wVnaTvB1BPy7dXsYT0/QGsTdu/nvotjWLDJvUSutfA3x4LrTecwL6fCtz8/gyG+aMhVef6mQAAAABJRU5ErkJggg==" }]
