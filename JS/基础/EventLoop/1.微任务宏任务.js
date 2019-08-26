2ef930a897dee3e7c9acf8d9250e7c7305153179
ReliaMM <1820184044@qq.com>
2019-08-26 10:03:42 +0800

    .

--- JS/基础/EventLoop/1.微任务宏任务.js
+++ JS/基础/EventLoop/1.微任务宏任务.js
@@ -1,10 +1,10 @@
-setTimeout(()=>{
-  console.log(1)
-},0)
-Promise.resolve().then(()=>{
-  console.log(2)
-})
-console.log(3)
+// setTimeout(()=>{
+//   console.log(1)
+// },0)
+// Promise.resolve().then(()=>{
+//   console.log(2)
+// })
+// console.log(3)
 
-// 这个是属于Eventloop的问题 main script 运行结束后，会有微任务队列和宏任务队列。微任务先执行，之后是宏任务。
+// // 这个是属于Eventloop的问题 main script 运行结束后，会有微任务队列和宏任务队列。微任务先执行，之后是宏任务。
 
