b1cf8dd38253b659fece6ee3c133d8fc27d07b88
ReliaMM <1820184044@qq.com>
2021-07-27 18:59:28 +0800

    feat: interview 2021-07

---
+++ 错题本/call.js
@@ -0,0 +1,14 @@
+var name = 'window'
+
+var person1 = {
+    name: 'person1',
+    foo4: function () {
+        return () => {
+            console.log(this.name)
+        }
+    }
+}
+
+var person2 = { name: 'person2' }
+
+person1.foo4().call(person2)  // person1
---
+++ 错题本/fn.js
@@ -0,0 +1,16 @@
+var i = 20;
+function fn() {
+    // 经过fn()(4)函数的调用，此时i是18
+    i -= 2; // 17
+    return function (n) {// 传入参数5
+        console.log(++i - n);  //18+1-5=14 此时i值是19 // 18 - 4
+    };
+}
+
+var f = fn(); 
+f(1);  
+f(2);  
+fn()(3);
+fn()(4);
+f(5); 
+console.log(i); 
