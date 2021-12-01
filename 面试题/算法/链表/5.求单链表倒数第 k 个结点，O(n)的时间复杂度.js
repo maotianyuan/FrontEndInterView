d6900b63b77c7703a659905bd39956f2eed580b2
ReliaMM <1820184044@qq.com>
2021-12-01 23:18:54 +0800

    链表

--- 面试题/算法/链表/5.求单链表倒数第 k 个结点，O(n)的时间复杂度.js
+++ 面试题/算法/链表/5.求单链表倒数第 k 个结点，O(n)的时间复杂度.js
@@ -1,5 +1,12 @@
-// 4.删除链表中倒数第 N 个节点
+// 5.求单链表倒数第 k 个结点，O(n)的时间复杂度
 
+// 思路一
+// 定义两个指针：
+// 第一个指针从链表的头指针开始遍历向前走k-1步，
+// 第二个指针保持不动；从第k步开始，第二个指针也开始从链表的头指针开始遍历；由于两个指针的距离保持在k-1，当第一个指针到达链表的尾结点时，第二个指针正好指向倒数第k个节点。
+
+// 思路二
+// 先遍历链表获取链表长度length，然后从头结点开始往后走length-k+1步
 class NodeItem {
   constructor(value, next) {
     this.value = value;
@@ -45,16 +52,7 @@ class List {
       num++;
     }
   }
-  print() {
-    let current = this.head;
-    let str = "";
-    while (current) {
-      str += current.value + "->";
-      current = current.next;
-    }
-    str += "null";
-    return str;
-  }
+  /** 删除倒数第N个节点：方式一 */
   removeFrontEnd(k) {
     let num = this.size - k - 1;
     let current = this.head;
@@ -64,6 +62,57 @@ class List {
     current.next = current.next.next;
     console.log(current);
     return this.head;
+  }
+  /** 获取倒数N 个节点，方式一 */
+  getFrontEndNode(k) {
+    let num = this.size - k;
+    let current = this.head;
+    while (num--) {
+      current = current.next;
+    }
+    return current;
+  }
+  /** 获取倒数N 个节点，方式二 */
+  getFrontEndNodeTypeTwo(k) {
+    let num = k - 1;
+    let slow = this.head;
+    let fast = this.head;
+
+    while (num--) {
+      fast = fast.next;
+    }
+    while (fast.next) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+    return slow;
+  }
+  /** 删除倒数第N个节点：方式二 */
+  removeFrontEndTypeTwo(k) {
+    let num = k - 1;
+    let slow = this.head;
+    let fast = this.head;
+
+    while (num--) {
+      fast = fast.next;
+    }
+    while (fast.next.next) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+    slow.next = slow.next.next;
+    // return slow;
+    return this.head;
+  }
+  print() {
+    let current = this.head;
+    let str = "";
+    while (current) {
+      str += current.value + "->";
+      current = current.next;
+    }
+    str += "null";
+    return str;
   }
 }
 
@@ -105,18 +154,8 @@ node.add(b);
 node.add(c);
 node.add(d);
 
-// 删除
-// node.remove(0);
-// console.log(node.print());
-
-// 反转
-const invertNode = invertLink(node.head);
-console.log(invertNode); // d\c\b\a
-
 // 翻转后删除第几个 = 删除链表倒是第 N 个
-const result = removeIndexLink(invertNode, 0);
-console.log(result);
-
-// 翻转后删除第几个 = 删除链表倒数第 N 个
-const result = node.removeFrontEnd(2);
+// const result = node.getFrontEndNode(2);
+// const result = node.getFrontEndNodeTypeTwo(2);
+const result = node.removeFrontEndTypeTwo(2);
 console.log(result);
