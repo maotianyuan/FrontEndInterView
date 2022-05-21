c2addcdfc974af21ce172736b771af687baa543d
ReliaMM <1820184044@qq.com>
2022-05-21 10:42:42 +0800

    .

--- 面试题/手写/5.实现一个 flat.js
+++ 面试题/手写/5.实现一个 flat.js
@@ -4,7 +4,7 @@ const arr = [1, 2, 3, 4, [12, 1212, [12]], 98, 1100];
 const flatten = (arr, level) => {
   return arr.reduce((prev, next) => {
     if (Array.isArray(next) && level) {
-      return [...prev, ...flatten(next), level--];
+      return [...prev, ...flatten(next, level--)];
     }
     prev.push(next);
     return prev;
